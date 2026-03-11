781c31633dd6685d7cadaea2267125f6a19c32be
MyTestClass301 <dkq4w25s7t@privaterelay.appleid.com>
2026-03-11 18:37:30 +0500

    Update test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,4 +3,8 @@ from selenium.webdriver.common.by import By
 
 # Use By.ID
 def test_add_task(driver):
-    pass
+    driver.find_element(By.ID, "taskInput").send_keys("Test task")
+    driver.find_element(By.ID, "addBtn").click()
+    tasks = driver.find_elements(By.TAG_NAME, "li")
+
+    assert len(tasks) == 1
